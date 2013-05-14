8dc34ac384988861f3a22d55d57c5088300f8699
Hersonls <hersonls@gmail.com>
2013-05-14 11:33:33 -0300

    Fixed missed structure.less include, added auto start in sliders and added highlight slider background

---
+++ static/js/scripts.js
@@ -0,0 +1,15 @@
+//
+//
+//
+
+!function($, window, document, undefined){
+    
+    $('#main-slider').carousel({
+        interval: 3000
+    });
+
+    $('#highlight-edition, #most-readed').carousel({
+        interval: 5000
+    });
+
+}(window.jQuery || window.ender, window, document);
